8202f12af26b9875613fcbd91019831f0cb93023
TrevisTJ <sasso.021015@gmail.com>
2024-11-18 17:25:44 -0300

    Improve logs

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"log"
 	"github.com/TrevisTJ/api-students/api"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
@@ -10,6 +10,6 @@ func main() {
 	server.ConfigureRoutes()
 
 	if err := server.Start(); err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("Failed to start server")
 	}
 }
